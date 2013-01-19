20f6df0758e21afdaa94b0b8d476dd6c9a868557
Ubuntu <ubuntu@ip-10-242-234-107.ec2.internal>
2013-01-19 16:47:50 +0000

    Creating MP0 repo with sample Java file

---
+++ mp0/java/HelloWorld.java
@@ -0,0 +1,11 @@
+// TEAM 17
+class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("<TEAM 17>");
+		System.out.println("Hello, world");
+		System.out.println("</TEAM 17>");
+
+	}
+}
