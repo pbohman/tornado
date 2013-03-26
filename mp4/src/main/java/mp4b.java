bce40c8249bebea49415f91245206f029be51b31
Josh Haycraft <joshayc@gmail.com>
2013-03-26 00:54:14 -0500

    Added file read and push to HBase. Had to force dependencies into combined .jar to avoid messing with classpaths. Perhaps this could be changed, but for now it serves it's purpose, even though we have a large .jar to deploy to AWS.

--- mp4/src/main/java/mp4b.java
+++ mp4/src/main/java/mp4b.java
@@ -1,28 +1,20 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Josh Haycraft
- * Date: 3/24/13
- * Time: 4:13 PM
- * To change this template use File | Settings | File Templates.
- */
-
-import java.io.IOException;
+import java.io.*;
+import java.util.Hashtable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class mp4b {
     public static void main(String[] args) throws IOException {
-        Configuration config = HBaseConfiguration.create();
+
+        //if (args.length  == 0)
+            //return;
 
         /* Write a java program that creates a table called “MetaHumans”.
         The program should create 2 column families to hold the data.
@@ -31,20 +23,106 @@ public class mp4b {
         The second column family should be named “powers”
             and contain the qualifiers corresponding to their power name, ex. “powers:supervision” . */
 
+        Configuration config = HBaseConfiguration.create();
         String sTableMetaHumans = "MetaHumans";
+        String[] sColumnFamiliesBackgroundPowers = new String[] { "background", "powers"};
 
+        BuildSchema(config, sTableMetaHumans, sColumnFamiliesBackgroundPowers);
+        LoadData(config, sTableMetaHumans, sColumnFamiliesBackgroundPowers, args[0]);
+
+
+    }
+
+    public static void BuildSchema(Configuration config, String sTable, String[] sColumnFamilies)    throws IOException
+    {
         HBaseAdmin admin = new HBaseAdmin(config);
-        if (admin.tableExists(sTableMetaHumans)) {
-            if (!admin.isTableDisabled(sTableMetaHumans))
-                admin.disableTable(sTableMetaHumans);
+        if (admin.tableExists(sTable)) {
+            if (!admin.isTableDisabled(sTable))
+                admin.disableTable(sTable);
 
-            admin.deleteTable(sTableMetaHumans);
+            admin.deleteTable(sTable);
         }
 
-        HTableDescriptor htd = new HTableDescriptor(sTableMetaHumans);
-        htd.addFamily(new HColumnDescriptor("background"));
-        htd.addFamily(new HColumnDescriptor("powers"));
+        HTableDescriptor htd = new HTableDescriptor(sTable);
+        for (String sColumnFamily : sColumnFamilies)
+        {
+            htd.addFamily(new HColumnDescriptor(sColumnFamily));
+        }
 
         admin.createTable(htd);
+    }
+
+    public static void LoadData(Configuration config, String sTable, String[] sColumnFamilies, String sPath)    throws IOException
+    {
+
+        BufferedReader reader = null;
+
+        Hashtable<String, String> hashFamilies = new Hashtable<String, String>();
+        HTable table = new HTable(config, sTable);
+
+        try {
+
+
+            hashFamilies.put("id", "background");
+            hashFamilies.put("gender", "background");
+            hashFamilies.put("race", "background");
+            hashFamilies.put("side", "background");
+            hashFamilies.put("superspeed", "powers");
+            hashFamilies.put("superhearing", "powers");
+            hashFamilies.put("supervision", "powers");
+            hashFamilies.put("flying", "powers");
+            hashFamilies.put("invisibility", "powers");
+            hashFamilies.put("icecontrol", "powers");
+            hashFamilies.put("superhealing", "powers");
+            hashFamilies.put("magicpowers", "powers");
+            hashFamilies.put("spidersense", "powers");
+            hashFamilies.put("superbrain", "powers");
+
+
+            String sLine;
+            Integer iColumnCount = 0;
+            reader = new BufferedReader(new FileReader(sPath));
+            if ((sLine = reader.readLine()) != null)
+            {
+                // header
+                // System.out.println(sLine);
+                String[] header = sLine.split(" ");
+
+                // data
+                while ((sLine = reader.readLine()) != null) {
+
+                    String[] data = sLine.split(" ");
+
+                    if(data.length > 0)
+                    {
+                        Put rowAdd = new Put(Bytes.toBytes(data[0]));
+                        iColumnCount = 1;
+                        while (iColumnCount < data.length)
+                        {
+                            rowAdd.add(Bytes.toBytes(hashFamilies.get(header[iColumnCount])),
+                                    Bytes.toBytes(header[iColumnCount]), Bytes
+                                    .toBytes(data[iColumnCount]));
+
+                            iColumnCount++;
+                        }
+                        if (iColumnCount > 1)
+                            table.put(rowAdd);
+
+                    }
+                }
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
     }
 }
