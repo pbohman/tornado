cf74c404b58ba15e9a796df5bb4cb71a9d4ee73a
Josh Haycraft <josh@iftruereturnzero.com>
2013-04-26 20:36:24 -0500

    Split Metric into MetricRate and MetricBurst, prepare for Burst chart

--- mp5/webserver/TornadoViewer/TornadoViewer/Controllers/HomeController.cs
+++ mp5/webserver/TornadoViewer/TornadoViewer/Controllers/HomeController.cs
@@ -30,7 +30,7 @@ namespace CassandraViewer.Controllers
         {
 
 
-            Highcharts chart = new Highcharts("chart")
+            Highcharts chart = new Highcharts("rate")
                 .InitChart(new Chart { DefaultSeriesType = ChartTypes.Spline })
                 .SetOptions(new GlobalOptions { Global = new Global { UseUTC = false } })
                 .SetTitle(new Title { Text = "Packets per destination addresses" })
@@ -62,7 +62,47 @@ namespace CassandraViewer.Controllers
                                            }*/
                                 })
             ;
-            chart.SetSeries(CassandraViewer.Models.Metric.GetData(DateTime.MinValue.Ticks, DateTime.MaxValue.Ticks));
+            chart.SetSeries(CassandraViewer.Models.MetricRate.GetData(DateTime.MinValue.Ticks, DateTime.MaxValue.Ticks));
+
+            return View(chart);
+        }
+
+        public ActionResult Burst()
+        {
+
+            Highcharts chart = new Highcharts("burst")
+                .InitChart(new Chart { DefaultSeriesType = ChartTypes.Spline })
+                .SetOptions(new GlobalOptions { Global = new Global { UseUTC = false } })
+                .SetTitle(new Title { Text = "Sources per destination addresses per second" })
+                .SetSubtitle(new Subtitle { Text = "Storm-aggregated metrics" })
+                .SetXAxis(new XAxis
+                {
+                    Type = AxisTypes.Datetime,
+                    TickmarkPlacement = Tickmarks.On,
+                })
+                .SetYAxis(new YAxis
+                {
+                    Title = new XAxisTitle { Text = "Unique IPs" },
+                    Min = 0
+                })
+                .SetTooltip(new Tooltip { Formatter = "function() { return '<b>'+ this.series.name +'</b><br/>'+ Highcharts.dateFormat('%F %n %F', this.x) +': '+ this.y +''; }" })
+                .SetPlotOptions(new PlotOptions
+                {
+                    /*
+                    Area = new PlotOptionsArea
+                           {
+                               Stacking = Stackings.Normal,
+                               LineColor = ColorTranslator.FromHtml("#666666"),
+                               LineWidth = 1,
+                               Marker = new PlotOptionsAreaMarker
+                                        {
+                                            LineWidth = 1,
+                                            LineColor = ColorTranslator.FromHtml("#666666")
+                                        }
+                           }*/
+                })
+            ;
+            chart.SetSeries(CassandraViewer.Models.MetricBurst.GetData(DateTime.MinValue.Ticks, DateTime.MaxValue.Ticks));
 
             return View(chart);
         }
--- mp5/webserver/TornadoViewer/TornadoViewer/Models/Metric.cs
+++ mp5/webserver/TornadoViewer/TornadoViewer/Models/Metric.cs
@@ -18,7 +18,7 @@ using MongoDB.Driver.Linq;
 
 namespace CassandraViewer.Models
 {
-    public class Metric
+    public class MetricRate
     {
         public ObjectId Id { get; set; }
         public string Hostname { get; set; }
@@ -47,9 +47,63 @@ namespace CassandraViewer.Models
             var client = new MongoClient(connectionString);
             var server = client.GetServer();
             var database = server.GetDatabase(ConfigurationManager.AppSettings["MongoDatabase"]);
-            var collection = database.GetCollection<Metric>(ConfigurationManager.AppSettings["MongoCollection"]);
+            var collection = database.GetCollection<MetricRate>(ConfigurationManager.AppSettings["MongoCollectionRate"]);
 
-            var query = collection.AsQueryable<Metric>();
+            var query = collection.AsQueryable<MetricRate>();
+            var categories = query.Select(x => x.Hostname).Distinct();
+
+            var dtStart = (start == DateTime.MinValue.Ticks) ? query.Min(x => x.Timestamp) : start;
+            var dtEnd = (end == DateTime.MinValue.Ticks) ? query.Max(x => x.Timestamp) : end;
+
+            List<Series> series = new List<Series>();
+
+            foreach (var c in categories)
+            {
+                var q = query
+                    .Where(x => x.Hostname.Equals(c) && x.Timestamp >= dtStart && x.Timestamp <= dtEnd).ToArray();
+
+                var g = q.GroupBy(a => a.Timestamp)
+                    .Select(y =>
+                                new object[] 
+                                { 
+                                    new DateTime(unixEpoch.Ticks).AddSeconds((double) y.Key).ToLocalTime(), 
+                                    y.Sum(z => z.Value)
+                                }
+                            );
+
+                series.Add(new Series { Name = c, Data = new Data(g.ToArray()) });
+            }
+
+            return series.ToArray();
+        }
+    }
+
+    public class MetricBurst
+    {
+        public ObjectId Id { get; set; }
+        public string Hostname { get; set; }
+        public long Timestamp { get; set; }
+        public int Value { get; set; }
+
+        public static Series[] GetData(long start, long end)
+        {
+            DateTime unixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+            ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["MongoConnection"];
+
+            var connectionString = "mongodb://localhost";
+
+            if (settings != null)
+                connectionString = settings.ConnectionString;
+
+
+
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            var database = server.GetDatabase(ConfigurationManager.AppSettings["MongoDatabase"]);
+            var collection = database.GetCollection<MetricRate>(ConfigurationManager.AppSettings["MongoCollectionBurst"]);
+
+            var query = collection.AsQueryable<MetricRate>();
             var categories = query.Select(x => x.Hostname).Distinct();
 
             var dtStart = (start == DateTime.MinValue.Ticks) ? query.Min(x => x.Timestamp) : start;
